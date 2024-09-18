907d3f1f573aee224118573c10c2fad18826c495
AayanTanvir <k98778376@gmail.com>
2024-09-18 06:15:22 -0400

    books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,15 +12,20 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+
+    if(book){ //if the book with the ISBN exists
+        res.send(JSON.stringify(book, null, 4));
+    } else { //if the book with the specified ISBN doesn't exist give an error
+        req.status(404).json({message: "Book does not exist"});
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
