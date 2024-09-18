1722e18996a3ed45b625461dad07f351ae97982c
AayanTanvir <k98778376@gmail.com>
2024-09-18 09:33:46 -0400

    book by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -29,8 +29,16 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const book = books.filter((books) => {
+        return books.author === author;
+    })
+
+    if(book) {
+        res.send(JSON.stringify(book, null, 4));
+    } else {
+        res.status(404).json({message: "the author does not exist"});
+    }
 });
 
 // Get all books based on title
