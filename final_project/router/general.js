d30464ab5444a08600b3d7c90e8500ec91f77166
AayanTanvir <k98778376@gmail.com>
2024-09-19 15:54:33 +0500

    task10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const axios = require('axios');
+const fs = require('fs').promises;
+const path = require('path');
 let books = require("./booksdb.js");
+const { rejects } = require('assert');
+const { read } = require('fs');
+const { error } = require('console');
+const { promisify } = require('util');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -25,44 +32,71 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+    Promise.resolve(books) 
+        .then((booksData) => {
+           return res.send(JSON.stringify(booksData, null, 4));
+        })
+        .catch((error) => {
+           return res.status(500).send(error);
+        })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    const book = books[isbn];
-
-    if(book){ //if the book with the ISBN exists
-        res.send(JSON.stringify(book, null, 4));
-    } else { //if the book with the specified ISBN doesn't exist give an error
-        req.status(404).json({message: "Book does not exist"});
-    }
+    
+    Promise.resolve()
+        .then(() => {
+            const book = books[isbn];
+        
+            if(book){ //if the book with the ISBN exists
+                res.send(JSON.stringify(book, null, 4));
+            } else { //if the book with the specified ISBN doesn't exist give an error
+                req.status(404).json({message: "Book does not exist"});
+            }
+        }) 
+        .catch((error) => {
+            return res.status(500).send(error);
+        })
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author.trim();
-    for(let key in books) {
-        const bookAuthor = books[key].author.replace(/\s+/g, '');
-        if(bookAuthor === author.replace(/\s+/g, '')) {
-            return res.json(books[key]);
-        }
-    }
-    return res.status(404).json({message: "author not found"});
+    
+    Promise.resolve()
+        .then(() => {
+            for(let key in books) {
+                const bookAuthor = books[key].author.replace(/\s+/g, '');
+                if(bookAuthor === author.replace(/\s+/g, '')) {
+                    return res.json(books[key]);
+                }
+            }
+            return res.status(404).json({message: "author not found"});
+        })
+        .catch((error) => {
+            return res.status(500).send(error);
+        })
 
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title.trim();
-    for(let key in books) {
-        const bookTitle = books[key].title;
-        if(bookTitle === title){
-            return res.json(books[key]);
-        }
-    }
-    return res.status(404).json({message: "title not found"});
+
+    Promise.resolve()
+        .then(() => {
+            for(let key in books) {
+                const bookTitle = books[key].title;
+                if(bookTitle === title){
+                    return res.json(books[key]);
+                }
+            }
+            return res.status(404).json({message: "title not found"});
+        })
+        .catch((error) => {
+           return res.status(500).send(error);
+        })
 
 });
 
